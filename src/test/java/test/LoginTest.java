02f5eb3625e478005b3a94be40c522f4dc34a432
Kirill Grishin <Rigo656@mail.ru>
2022-06-07 20:06:38 +0300

    Initial commit

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import data.DataHelper;
+import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTest {
+    @AfterAll
+    static void deletingDataFromTheDb() {
+        DataHelper.DeleteInfo.deletingData();
+    }
+
+    @Test
+    public void shouldAuthorizationIsSuccessful() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:7777");
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.VerificationCode.getAuthCode(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    public void shouldGiveErrorIfPasswordInvalid() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:7777");
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getInvalidAuthInfo();
+        loginPage.invalidLogin(authInfo);
+        loginPage.getNotificationAuth();
+    }
+
+    @Test
+    public void shouldGiveLockIfAnInvalidPasswordIsEnteredThreeTimes() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:7777");
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getInvalidAuthInfo();
+        loginPage.invalidPasswordThreeTimes(authInfo);
+        loginPage.getNotification();
+    }
+}
